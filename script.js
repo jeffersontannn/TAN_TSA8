b466ef3ae14c2221d558fa66507c5615e0d0ad31
jeffersontannn <167377050+jeffersontannn@users.noreply.github.com>
2024-06-28 09:58:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('reset').addEventListener('click', () => {
-    document.querySelector('.flexbox').style = '';
+    document.querySelector('.flexbox').style.flexDirection = 'row';
+    document.querySelector('.flexbox').style.justifyContent = 'flex-start';
+    document.querySelector('.flexbox').style.alignItems = 'stretch';
+    document.querySelector('.flexbox').style.gap = '0px';
+    document.querySelectorAll('.box').forEach(box => box.style.flexGrow = 0);
     document.getElementById('gap').value = 0;
     document.getElementById('box1').value = 0;
     document.getElementById('box2').value = 0;
     document.getElementById('box3').value = 0;
-});
-
-document.getElementById('gap').addEventListener('input', (event) => {
-    document.querySelector('.flexbox').style.gap = `${event.target.value}px`;
 });
 
 document.getElementById('row').addEventListener('click', () => {
@@ -54,6 +54,10 @@ document.getElementById('align-end').addEventListener('click', () => {
     document.querySelector('.flexbox').style.alignItems = 'flex-end';
 });
 
+document.getElementById('gap').addEventListener('input', (e) => {
+    document.querySelector('.flexbox').style.gap = e.target.value + 'px';
+});
+
 document.getElementById('grow-reset').addEventListener('click', () => {
     document.querySelectorAll('.box').forEach(box => box.style.flexGrow = 0);
     document.getElementById('box1').value = 0;
@@ -63,19 +67,16 @@ document.getElementById('grow-reset').addEventListener('click', () => {
 
 document.getElementById('grow-all').addEventListener('click', () => {
     document.querySelectorAll('.box').forEach(box => box.style.flexGrow = 1);
-    document.getElementById('box1').value = 1;
-    document.getElementById('box2').value = 1;
-    document.getElementById('box3').value = 1;
 });
 
-document.getElementById('box1').addEventListener('input', (event) => {
-    document.querySelector('#flexbox1').style.flexGrow = event.target.value;
+document.getElementById('box1').addEventListener('input', (e) => {
+    document.getElementById('flexbox1').style.flexGrow = e.target.value;
 });
 
-document.getElementById('box2').addEventListener('input', (event) => {
-    document.querySelector('#flexbox2').style.flexGrow = event.target.value;
+document.getElementById('box2').addEventListener('input', (e) => {
+    document.getElementById('flexbox2').style.flexGrow = e.target.value;
 });
 
-document.getElementById('box3').addEventListener('input', (event) => {
-    document.querySelector('#flexbox3').style.flexGrow = event.target.value;
+document.getElementById('box3').addEventListener('input', (e) => {
+    document.getElementById('flexbox3').style.flexGrow = e.target.value;
 });
